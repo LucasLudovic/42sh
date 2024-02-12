0d31cee5d5f14c84c6a5dd74d111c08d2b0a4a41
Valiance <lucas.ludovic@epitech.eu>
2024-02-12 10:44:42 +0100

    [UPDATE] Adding the basics functions to implement

--- src/my_shell.c
+++ src/my_shell.c
@@ -5,11 +5,27 @@
 ** Shell for the minishell project
 */
 
+#include <stddef.h>
+#include <unistd.h>
 #include "my_macros.h"
+
+static
+void print_prompt(void)
+{
+    write(1, "> ", 2);
+}
 
 int my_shell(int number_of_arguments, char **arguments, char **env)
 {
-    int shell_alive = 1;
-    while ()
+    int shell_alive = TRUE;
+    char *command_user = NULL;
+    char **shell_env;
+
+    shell_environment = get_environment();
+    while (shell_alive) {
+        print_prompt();
+        get_usr_command();
+        exec_usr_command();
+    }
     return SUCCESS;
 }
