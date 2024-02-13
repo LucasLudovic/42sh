ae0872d58a436f21d540c998b5664214a882eaaa
Valiance <lucas.ludovic@epitech.eu>
2024-02-13 19:02:38 +0100

    [UPDATE] Change the directory to HOME when no arguments is provided

---
+++ include/builtin/cd.h
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2024
+** Cd
+** File description:
+** Header to change the current directory
+*/
+
+#ifndef CD_H_
+    #define CD_H_
+
+#endif
--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -5,11 +5,36 @@
 ** Change the directory for the minishell project
 */
 
+#include <unistd.h>
+#include <stddef.h>
 #include "shell/my_shell.h"
 #include "my_macros.h"
+#include "my.h"
 
-int change_directory(UNUSED shell_t *shell, UNUSED char **arguments,
-    UNUSED int nb_arguments)
+static
+char *get_home(environment_t *environment)
 {
+    while (environment != NULL) {
+        if (my_strcmp(environment->key, "HOME") == 0)
+            return environment->value;
+        environment = environment->next;
+    }
+    return NULL;
+}
+
+int change_directory(shell_t *shell, char **arguments, int nb_arguments)
+{
+    char *home_directory = NULL;
+
+    if (shell == NULL || shell->environment == NULL)
+        return FAILURE;
+    home_directory = get_home(shell->environment);
+    if (home_directory == NULL)
+        return FAILURE;
+    if (nb_arguments == 1) {
+        if (chdir(home_directory) != 0)
+            return display_error("Unable to change the directory");
+        return SUCCESS;
+    }
     return SUCCESS;
 }
