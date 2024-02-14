20b1e67c78014aaf6b3cae0ef687a43b479af266
Valiance <lucas.ludovic@epitech.eu>
2024-02-14 14:31:37 +0100

    [UPDATE] Which return the correct path

--- src/dependencies/which.c
+++ src/dependencies/which.c
@@ -27,6 +27,7 @@ static
 int check_file_in_dir(DIR *directory, const char *file_name)
 {
     struct dirent *file = readdir(directory);
+
     while (file != NULL) {
         if (my_strcmp(file_name, file->d_name))
             return TRUE;
@@ -36,7 +37,8 @@ int check_file_in_dir(DIR *directory, const char *file_name)
 }
 
 static
-char *get_file_path(DIR *directory, char *solo_path, const char *file_name)
+char *get_file_path(DIR *directory, const char *solo_path,
+    const char *file_name)
 {
     char *absolute_path = NULL;
     int len_absolute_path = 0;
@@ -49,7 +51,6 @@ char *get_file_path(DIR *directory, char *solo_path, const char *file_name)
     absolute_path = malloc(sizeof(char) * (len_absolute_path + 1));
     my_strcpy(absolute_path, solo_path);
     my_strcat(absolute_path, file_name);
-    my_putstr(absolute_path);
     return absolute_path;
 }
 
@@ -75,14 +76,17 @@ char *retrieve_function_path(char *path, const char *file_name)
     return NULL;
 }
 
-int get_function_absolute_path(environment_t *environment, const char **arguments)
+char *get_function_absolute_path(environment_t *environment,
+    const char **arguments)
 {
     char *path = NULL;
-    char *function_path = NULL;
+    char *function_absolute_path = NULL;
 
     path = get_path(environment);
     if (path == NULL)
-        return FAILURE;
-    free(path);
-    return SUCCESS;
+        return NULL;
+    function_absolute_path = retrieve_function_path(path, arguments[0]);
+    if (path != NULL)
+        free(path);
+    return function_absolute_path;
 }
