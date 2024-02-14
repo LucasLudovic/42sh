743fd07453d38709a55c6a647999972cb5f74bef
Valiance <lucas.ludovic@epitech.eu>
2024-02-14 20:45:26 +0100

    [UPDATE] Adding the exit message when exit

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -8,11 +8,13 @@
 #include "dependencies/environment.h"
 #include "shell/my_shell.h"
 #include "my_macros.h"
+#include "my.h"
 
 int exit_shell(shell_t *shell, UNUSED char **arguments, int nb_arguments)
 {
     if (nb_arguments != 1)
         return FAILURE;
     shell->alive = FALSE;
+    my_putstr("exit\n");
     return SUCCESS;
 }
