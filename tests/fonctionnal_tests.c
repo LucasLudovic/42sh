3c0194e82fea993ee0b7a6a9bc3da5225aa0750f
Louka Ortega Cand <louka.ortega-cand@epitech.eu>
2024-05-10 17:03:57 +0200

    [UPDATE] add 30% to line coverage for tests

--- tests/fonctionnal_tests.c
+++ tests/fonctionnal_tests.c
@@ -66,28 +66,28 @@ Test(cd, example1, .init = cr_redirect_stdout) {
     close(input_fd);
 }
 
-// Test(cd , example2, .init = cr_redirect_stderr) {
-//     char *env[] = {
-//         "SHELL=/usr/bin/zsh",
-//         "HYPRLAND_CMD=Hyprland",
-//         "XDG_BACKEND=wayland",
-//         "DESKTOP_SESSION=hyprland",
-//         "XCURSOR_SIZE=24",
-//         "XDG_SEAT=seat0",
-//         "PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin"
-//         "LESS=-R",
-//         "LSCOLORS=Gxfxcxdxbxegedabagacad",
-//         NULL
-//     };
+Test(cd , example2, .init = cr_redirect_stderr) {
+    char *env[] = {
+        "SHELL=/usr/bin/zsh",
+        "HYPRLAND_CMD=Hyprland",
+        "XDG_BACKEND=wayland",
+        "DESKTOP_SESSION=hyprland",
+        "XCURSOR_SIZE=24",
+        "XDG_SEAT=seat0",
+        "PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin"
+        "LESS=-R",
+        "LSCOLORS=Gxfxcxdxbxegedabagacad",
+        NULL
+    };
 
-//     int input_fd = setup_input_pipe("cd 42sh\n");
-//     dup2(input_fd, STDIN_FILENO);
-//     int ret = my_shell(env);
-//     fflush(stdout);
-//     cr_assert_stdout_eq_str("42sh: Not a directory.\n");
+    int input_fd = setup_input_pipe("cd 42sh\n");
+    dup2(input_fd, STDIN_FILENO);
+    my_shell(env);
+    fflush(stdout);
+    cr_assert_stderr_eq_str("42sh: Not a directory.\n");
 
-//     close(input_fd);
-// }
+    close(input_fd);
+}
 
 Test(env, example1, .init = cr_redirect_stdout) {
     char *env[] = {
@@ -122,23 +122,6 @@ Test(setenv, example1, .init = cr_redirect_stdout) {
     cr_assert_stdout_eq_str("SHELL=/usr/bin/zsh\nHYPRLAND_CMD=Hyprland\nXDG_BACKEND=wayland\n");
     close(input_fd);
 }
-
-// Test(setenv, example2, .init = cr_redirect_stderr) {
-//     char *env[] = {
-//         "SHELL=/usr/bin/zsh",
-//         "HYPRLAND_CMD=Hyprland",
-//         "XDG_BACKEND=wayland",
-//         NULL
-//     };
-
-//     int input_fd = setup_input_pipe("setenv .VAR val\n");
-//     dup2(input_fd, STDIN_FILENO);
-//     int ret = my_shell(env);
-//     cr_assert_eq(ret, 0, "Expected return value is 0 but got %d", ret);
-//     fflush(stdout);
-//     cr_assert_stdout_eq_str("setenv: Variable name must begin with a letter.\n");
-//     close(input_fd);
-// }
 
 Test(setenv, example3, .init = cr_redirect_stdout) {
     char *env[] = {
@@ -190,3 +173,95 @@ Test(exit, example1, .init = cr_redirect_stdout) {
     cr_assert_stdout_eq_str("exit\n");
     close(input_fd);
 }
+
+Test(alias, example1, .init = cr_redirect_stdout) {
+    char *env[] = {
+        "SHELL=/usr/bin/zsh",
+        "HYPRLAND_CMD=Hyprland",
+        "XDG_BACKEND=wayland",
+        NULL
+    };
+
+    int input_fd = setup_input_pipe("alias caca=env ; caca\n");
+    dup2(input_fd, STDIN_FILENO);
+    int ret = my_shell(env);
+    cr_assert_eq(ret, 0, "Expected return value is 0 but got %d", ret);
+    fflush(stdout);
+    cr_assert_stdout_eq_str("SHELL=/usr/bin/zsh\nHYPRLAND_CMD=Hyprland\nXDG_BACKEND=wayland\n");
+    close(input_fd);
+}
+
+Test(set, example1, .init = cr_redirect_stdout) {
+    char *env[] = {
+        "SHELL=/usr/bin/zsh",
+        "HYPRLAND_CMD=Hyprland",
+        "XDG_BACKEND=wayland",
+        NULL
+    };
+
+    int input_fd = setup_input_pipe("set BIN = /usr/bin\n");
+    dup2(input_fd, STDIN_FILENO);
+    int ret = my_shell(env);
+    cr_assert_eq(ret, 0, "Expected return value is 0 but got %d", ret);
+    close(input_fd);
+}
+
+Test(single_right_redirection, example1, .init = cr_redirect_stdout) {
+    char *env[] = {
+        "SHELL=/usr/bin/zsh",
+        "HYPRLAND_CMD=Hyprland",
+        "XDG_BACKEND=wayland",
+        NULL
+    };
+
+    int input_fd = setup_input_pipe("env > /dev/null\n");
+    dup2(input_fd, STDIN_FILENO);
+    int ret = my_shell(env);
+    cr_assert_eq(ret, 0, "Expected return value is 0 but got %d", ret);
+}
+
+Test(double_right_redirection, example1, .init = cr_redirect_stdout) {
+    char *env[] = {
+        "SHELL=/usr/bin/zsh",
+        "HYPRLAND_CMD=Hyprland",
+        "XDG_BACKEND=wayland",
+        NULL
+    };
+
+    int input_fd = setup_input_pipe("env >> /dev/null\n");
+    dup2(input_fd, STDIN_FILENO);
+    int ret = my_shell(env);
+    cr_assert_eq(ret, 0, "Expected return value is 0 but got %d", ret);
+}
+
+/*
+Test(single_left_redirection, example1, .init = cr_redirect_stdout) {
+    char *env[] = {
+        "SHELL=/usr/bin/zsh",
+        "HYPRLAND_CMD=Hyprland",
+        "XDG_BACKEND=wayland",
+        NULL
+    };
+
+    int input_fd = setup_input_pipe("cat < Makefile\n");
+    dup2(input_fd, STDIN_FILENO);
+    int ret = my_shell(env);
+    cr_assert_eq(ret, 0, "Expected return value is 0 but got %d", ret);
+}
+
+Test(double_left_redirection, example1, .init = cr_redirect_stdout) {
+    char *env[] = {
+        "SHELL=/usr/bin/zsh",
+        "HYPRLAND_CMD=Hyprland",
+        "XDG_BACKEND=wayland",
+        NULL
+    };
+
+    int input_fd = setup_input_pipe("cat << END\nMakefile\nEND\n");
+    dup2(input_fd, STDIN_FILENO);
+    int ret = my_shell(env);
+    cr_assert_eq(ret, 0, "Expected return value is 0 but got %d", ret);
+}
+*/
+
+
